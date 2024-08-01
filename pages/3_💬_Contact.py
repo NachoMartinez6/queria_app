fd44a018278bf29c2ceda3b0842e5c7850f86ba7
jose.martinez.perez@accenture.com-at-735243292625 <jose.martinez.perez@accenture.com>
2024-08-01 20:15:42 +0200

    add a new page

---
+++ pages/3_💬_Contact.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.write("Hello World")
+
+# Inserta las imágenes en el título
+st.markdown(f'<h1>Contacto<h1>', unsafe_allow_html=True)
+st.markdown(f'<h3>Para cualquier tipo de consulta, contacta a nuestro perfil de Linkedin!</h3>', unsafe_allow_html=True)
+
+
+
