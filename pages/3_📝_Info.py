83a9d66685e9523b03ea063c65de45d2930ab98a
jose.martinez.perez@accenture.com-at-735243292625 <jose.martinez.perez@accenture.com>
2024-08-02 08:30:07 +0200

    Update text page 3

--- pages/3_📝_Info.py
+++ pages/3_📝_Info.py
@@ -3,7 +3,7 @@ import streamlit as st
 # st.write("Hello World")
 
 # Inserta las imágenes en el título
-st.markdown(f'<h2>Información del Proyecto<h2>', unsafe_allow_html=True)
+st.markdown(f'<h2>Información del Proyecto<h2>')
 st.markdown(''' El proyecto Queria fue diseñado como una DEMO para una formación centrada en analisis de datos.
 
 No se trata de una aplicación con fines comerciales, sino más bien divulgativa, el código esta público
@@ -12,10 +12,11 @@ en mi repositorio de Github para que cualquiera que lo desee pueda replicarlo.
 La API utilizada es la de OpenAI para hacer la llamada al LLM.
 
 Confío en que hayas probado la funcionalidad y te haya gustado el proyecto, ¡Muchas gracias por haber llegado hasta aquí!
-''', unsafe_allow_html=True)
+'''#, unsafe_allow_html=True
+)
 
 
-st.markdown(f'<h2>Agradecimientos</h2>', unsafe_allow_html=True)
+st.markdown(f'<h2>Agradecimientos</h2>')
 st.markdown('''En primer lugar agradecer a mi compañera [Olex]("https://www.linkedin.com/in/olexandrazaporozhets/") por su ayuda y comprensión
 durante todo el proyecto.
             
@@ -24,4 +25,5 @@ y por todo el apoyo realizado!
 
 Y por supuesto gracias a ti, por haber visto la aplicación, espero que os guste. Un saludo! 👋🏾)
 
-''',  unsafe_allow_html=True)
+'''#,  unsafe_allow_html=True
+)
